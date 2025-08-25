faaf5627d1d59879e1a1d35792a57ce15fd2b0e5
WilliamCCHuang <hang6318179@gmail.com>
2025-08-25 15:59:49 +0800

    commit

--- 15_trust_region_policy_optimization/a2c.py
+++ 15_trust_region_policy_optimization/a2c.py
@@ -145,3 +145,59 @@ def test_a2c(model, env, num_rounds=10, device='cpu'):
                 break
 
     return rewards / num_rounds, steps / num_rounds
+
+
+def train_ppo(model, trajectory, critic_optimizer, actor_optimizer, writer, step_idx, args, device):
+    states = torch.tensor([e[0].state for e in trajectory]).to(device)
+    actions = torch.tensor([e[0].action for e in trajectory]).to(device)
+    # obs_history = obs_history.to(device)  # (bs, 28)
+    # value_history = value_history.unsqueeze(-1).to(device)  # (bs, 1)
+    # action_history = action_history.to(device)  # (bs, 8)
+
+
+    return
+    critic_optimizer.zero_grad()
+    pred_values = model.predict_values(obs_history)  # (bs, 8), (bs, 8), (bs, 1)
+    loss_value = F.mse_loss(pred_values, value_history)
+    loss_value.backward()
+    critic_optimizer.step()
+
+    actor_optimizer.zero_grad()
+    mus = model.actor(obs_history)  # (32, 8)
+    adv_values = value_history - pred_values.detach()  # (32, 1)
+    log_pred_action_prob = adv_values * cal_log_policy(mus, model.actor.logstd, action_history)  # (32, 8)
+    loss_policy = - log_pred_action_prob.mean()
+    
+    loss_entropy = - cal_entropy(model.actor.logstd)
+
+    loss_actor = loss_policy + args.loss_entropy_coef * loss_entropy
+    loss_actor.backward()
+    actor_optimizer.step()
+
+    writer.add_scalar('loss_value', loss_value, step_idx)
+    writer.add_scalar('loss_policy', loss_policy, step_idx)
+    writer.add_scalar('loss_entropy', loss_entropy, step_idx)
+    writer.add_scalar('loss_actor', loss_actor, step_idx)
+
+
+def cal_gae(trajectory, model, gamma, gae_lambda, device):
+    states = torch.tensor([e[0].state for e in trajectory]).to(device)
+
+    with torch.no_grad():
+        values = model.predict_values(states)
+    values = values.detach().cpu().numpy()
+    
+    gae_values = []
+    for (exp,), value, next_value in zip(reversed(trajectory[:-1]), reversed(values[:-1]), reversed(values[1:])):
+        if exp.done:
+            delta = exp.reward - value
+            gae_value = delta
+        else:
+            delta = exp.reward + gamma * next_value - value
+            gae_value = delta + gamma * gae_lambda * gae_value
+        
+        gae_values.append(gae_value)
+    
+    gae_values.reverse()
+    gae_tensor = torch.tensor(gae_values).to(device)
+    return gae_tensor
---
+++ 15_trust_region_policy_optimization/train_ppo.py
@@ -0,0 +1,154 @@
+import argparse
+import numpy as np
+from pathlib import Path
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from atari_env import make_atari_env
+from a2c import A2C, train_a2c, test_a2c
+from experience import NStepExperienceMemory
+
+
+def pack_exp_into_batch(exp_batch, model, last_gamma, device):
+    obs_history = []
+    value_history = []
+    action_history = []
+    not_done_idx = []
+    last_obs = []
+    for idx, exp in enumerate(exp_batch):
+        obs_history.append(exp.obs)
+        value_history.append(exp.dis_reward)
+        action_history.append(exp.action)
+
+        # this exp is not done yet
+        if exp.last_obs is not None:
+            not_done_idx.append(idx)
+            last_obs.append(exp.last_obs)
+
+    obs_history = torch.tensor(np.array(obs_history)).to(device)
+    action_history = torch.tensor(np.array(action_history)).to(device)
+    value_history = torch.tensor(np.array(value_history)).float().to(device)
+
+    if not not_done_idx:
+        last_obs = torch.tensor(last_obs).to(device)
+
+        with torch.no_grad():
+            last_obs_values = model.predict_values(last_obs)
+        value_history[not_done_idx] += last_gamma * last_obs_values.detach()
+
+    return obs_history, value_history, action_history
+
+
+def main(args):
+    if not torch.cuda.is_available():
+        args.device = 'cpu'
+    device = torch.device(args.device)
+    print('Use device:', device)
+
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    exp_dir = Path(f'./{args.exp_name}')
+    exp_dir.mkdir(exist_ok=True)
+    writer = SummaryWriter(str(exp_dir))
+
+    env = make_atari_env(args.env_name)
+    test_env = make_atari_env(args.env_name)
+
+    model = A2C(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+    critic_optimizer = torch.optim.Adam(model.critic.parameters(), args.lr_critic)
+    actor_optimizer = torch.optim.Adam(model.actor.parameters(), args.lr_actor)
+    print(model)
+
+    exp_memory = NStepExperienceMemory(env, model, n_steps=1)
+
+    print()
+    print('Start training...')
+    step_idx = 0
+    best_test_episode_reward = -np.inf
+
+    if args.resume_path is not None:
+        state_dict = torch.load(args.resume_path, map_location=device, weights_only=False)
+        model.load_state_dict(state_dict['model'])
+        critic_optimizer.load_state_dict(state_dict['critic_optimizer'])
+        actor_optimizer.load_state_dict(state_dict['actor_optimizer'])
+        step_idx = state_dict['step']
+        best_test_episode_reward = state_dict['best_test_episode_reward']
+        print(f'Loaded resume. Continue training from the step {step_idx}. Best test episode reward: {best_test_episode_reward:.2f}')
+    
+    trajectory = []
+    for exps in exp_memory:
+        episode_reward_pairs = exp_memory.pop_episode_reward_step_pairs()
+        if episode_reward_pairs:
+            rewards, steps = list(zip(*episode_reward_pairs))
+
+            writer.add_scalar('episode/reward', rewards[0], step_idx)
+            writer.add_scalar('episode/steps', steps[0], step_idx)
+        
+        # collect exps until they are enough to pack a batch
+        trajectory.append(exps)
+        if len(trajectory) < args.trajectory_size:
+            continue
+        
+        last_gamma = args.discount_rate ** args.reward_steps
+        obs_history, value_history, action_history = pack_exp_into_batch(exp_batch, model, last_gamma, device)
+        exp_batch = []
+
+        train_a2c(model, obs_history, value_history, action_history, critic_optimizer, actor_optimizer, writer, step_idx, args, device)
+
+        step_idx += 1
+
+        # if training is done
+        if args.training_steps is not None and step_idx >= args.training_steps:
+            break
+
+        # do testing
+        if step_idx % args.test_steps == 0:
+            test_avg_reward, test_avg_steps = test_a2c(model, test_env, num_rounds=10, device=device)
+
+            if best_test_episode_reward < test_avg_reward:
+                best_test_episode_reward = test_avg_reward
+
+                file_name = exp_dir / f'best_a2c_{step_idx}_{best_test_episode_reward:.2f}.pt'
+                state_dict = {
+                    'step': step_idx,
+                    'model': model.state_dict(),
+                    'critic_optimizer': critic_optimizer.state_dict(),
+                    'actor_optimizer': actor_optimizer.state_dict(),
+                    'best_test_episode_reward': best_test_episode_reward,
+                }
+                torch.save(state_dict, file_name)
+
+            file_name = exp_dir / f'latest_a2c.pt'
+            state_dict = {
+                'step': step_idx,
+                'model': model.state_dict(),
+                'critic_optimizer': critic_optimizer.state_dict(),
+                'actor_optimizer': actor_optimizer.state_dict(),
+                'best_test_episode_reward': best_test_episode_reward,
+            }
+            torch.save(state_dict, file_name)
+            print(f'Step: {step_idx} | test steps: {test_avg_steps:.2f} | test reward: {test_avg_reward:.2f} | best test reward: {best_test_episode_reward:.2f}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--exp-name', type=str, default='exp_a2c')
+    parser.add_argument('--env-name', type=str, default='HalfCheetah-v4')
+    parser.add_argument('--num-env', type=int, default=16)
+    parser.add_argument('--resume-path', type=str, default=None)
+    parser.add_argument('--training-steps', type=int, default=1000000)
+    # parser.add_argument('--ppo-steps', type=int, default=1)
+    parser.add_argument('--trajectory-size', type=int, default=2048)
+    parser.add_argument('--discount-rate', type=float, default=0.99)
+    parser.add_argument('--bs', type=float, default=32)
+    parser.add_argument('--lr-actor', type=float, default=1e-5)
+    parser.add_argument('--lr-critic', type=float, default=1e-3)
+    parser.add_argument('--loss-entropy-coef', type=float, default=1e-3)
+    parser.add_argument('--test-steps', type=int, default=1000)
+    parser.add_argument('--seed', type=int, default=20)
+    args = parser.parse_args()
+
+    main(args)
