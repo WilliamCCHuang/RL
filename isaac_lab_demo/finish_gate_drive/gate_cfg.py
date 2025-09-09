7a75fc1718b4bff83200041f2cecab8c54678a38
WilliamCCHuang <hang6318179@gmail.com>
2025-09-09 21:51:07 +0800

    commit

--- isaac_lab_demo/finish_gate_drive/gate_cfg.py
+++ isaac_lab_demo/finish_gate_drive/gate_cfg.py
@@ -1,4 +1,22 @@
 import isaaclab.sim as sim_utils
+from isaaclab.assets import RigidObjectCfg
+
+FINISH_GATE_CFG = RigidObjectCfg(
+    prim_path="World/envs/env_.*/FinishGate",
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="/content/IsaacLab/source/isaaclab_tasks/isaaclab_tasks/direct/leatherback/custom_assets/finish_gate.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+    ),
+    init_state=RigidObjectCfg.InitialStateCfg()
+)
 
 FINISH_GATE_CFG = sim_utils.UsdFileCfg(
     usd_path="/content/IsaacLab/source/isaaclab_tasks/isaaclab_tasks/direct/leatherback/custom_assets/finish_gate.usd",
